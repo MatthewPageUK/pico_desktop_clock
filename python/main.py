473f99f6b57a1934ecab39dbc8a2b7a6f3adb166
Matthew Page <46349796+MatthewPageUK@users.noreply.github.com>
2022-12-29 19:21:24 +0000

    Code and time fixes - NTP and Wifi

--- python/main.py
+++ python/main.py
@@ -1,15 +1,20 @@
-import time
 import math
 import random
 import machine
+import utime
+import network
+import ntptime
 from picographics import PicoGraphics, DISPLAY_ROUND_LCD_240X240, PEN_P8
+
+WIFI_SSID = "yournetwork"
+WIFI_PASSWORD = "yourpassword"
 
 display = PicoGraphics(display=DISPLAY_ROUND_LCD_240X240, pen_type=PEN_P8, rotate=90)
 display.set_backlight(1.0)
 
 WIDTH, HEIGHT = display.get_bounds()
 MIDX = int(WIDTH/2)
-MIDY = int(HEIGHT/2)
+MIDY = int(WIDTH/2)
 FPS = 25
 BG = display.create_pen(90, 25, 70)
 PURPLE = display.create_pen(255, 50, 255)
@@ -18,28 +23,119 @@ BLACK = display.create_pen(0, 0, 0)
 STARS_TOTAL = 150
 STARS_SPEED_MAX = 50
 STARS_COLOUR = display.create_pen(220, 200, 220)
+DEFAULT_NOTIFICATION_DELAY = 0
 
+# Show a notification on the display
+def notification(text, colour, delay = DEFAULT_NOTIFICATION_DELAY, background = BG):
+    display.set_pen(background)
+    display.clear()
+    display.set_pen(colour)
+    display.text(text, int((WIDTH - display.measure_text(text, 2)) / 2), 110, 200, 2)
+    display.update()
+    utime.sleep(delay)
+
+# Show an alarm message
+def alarm(text):
+    for x in range(0, 5):
+        notification(text, PURPLE, 0.5)
+        notification(text, BG, 0.5, PURPLE)
+
+# Connect to WiFi and get time
+def setNtpTime(ssid, password):
+    notification("Setting time...", GREEN)
+
+    wifiNetwork = network.WLAN(network.STA_IF)
+    wifiNetwork.active(True)
+    wifiNetwork.connect(ssid, password)
+
+    notification("Connecting to WIFI...", GREEN)
+    wifiCount = 1
+    while wifiNetwork.status() != network.STAT_GOT_IP:
+        notification("Connecting... {c}".format(c=wifiCount), GREEN, 1)
+        wifiCount += 1
+
+    notification("Getting NTP time", GREEN)
+    ntptime.settime()
+
+    wifiNetwork.disconnect()
+    wifiNetwork.active(False)
+    notification("Done", PURPLE)
+
+    return True
+
+# Draw an outer ring
+def drawRing(value, distance, intervals = 60, small = 3, large = 6, inner = 1):
+    for dot in range(0, intervals):
+        angle = math.pi * 2 * (dot / intervals) - math.pi / 2
+        x = int(MIDX + math.cos(angle) * distance)
+        y = int(MIDY + math.sin(angle) * distance)
+
+        if dot >= value:
+            colour = GREEN
+        else:
+            colour = PURPLE
+
+        if dot == value:
+            radius = large
+        else:
+            radius = small
+
+        display.set_pen(colour)
+        display.circle(x, y, radius)
+        display.set_pen(BG)
+        display.circle(x, y, inner)
+
+# Draw a hand on the clock face
+def drawHand(value, distance, intervals = 60):
+    display.set_pen(GREEN)
+    angle = math.pi * 2 * (value / intervals) - math.pi / 2
+    x = int(MIDX + math.cos(angle) * distance)
+    y = int(MIDY + math.sin(angle) * distance)
+    display.line(MIDX, MIDY, x, y)
+
+# A star class
 class Star:
-    def __init__(self, x, y, r, dx, dy, pen):
+    def __init__(self, x = MIDX, y = MIDY, r = 1, dx = 0, dy = 0):
         self.x = x
         self.y = y
         self.r = r
         self.dx = dx
         self.dy = dy
-        self.pen = pen
+        self.rebirth()
+
+    # Move star and rebirth if off screen
+    def move(self):
+        self.x += self.dx / 10
+        self.y += self.dy / 10
+        if self.x < 0 or self.x > WIDTH or self.y < 0 or self.y > HEIGHT:
+            self.rebirth()
+    # Draw star on the display
+    def draw(self):
+        display.set_pen(STARS_COLOUR)
+        display.circle(int(self.x), int(self.y), int(self.r))
+
+    # Rebirth the star in the middle with random vector
+    def rebirth(self):
+        self.x = MIDX
+        self.y = MIDY
+        self.dx = random.randint(0, STARS_SPEED_MAX * 2) - STARS_SPEED_MAX
+        self.dy = random.randint(0, STARS_SPEED_MAX * 2) - STARS_SPEED_MAX
+
+notification("Starting up!", PURPLE)
+
+# Get the time
+setNtpTime(WIFI_SSID, WIFI_PASSWORD)
 
 # Create some stars
 stars = []
 for i in range(0, STARS_TOTAL):
-    dx = random.randint(0, STARS_SPEED_MAX * 2) - STARS_SPEED_MAX
-    dy = random.randint(0, STARS_SPEED_MAX * 2) - STARS_SPEED_MAX
-    stars.append(Star(MIDX, MIDY, 1, dx, dy, STARS_COLOUR))
+    stars.append(Star())
 
 # Main Loop
 while True:
 
     # Time in seconds
-    seconds = time.time()
+    seconds = utime.time()
 
     # Clock seconds, minutes and hours
     clock_seconds = seconds % (24 * 3600)
@@ -50,104 +146,28 @@ while True:
     if clock_hours > 12:
         clock_hours -= 12
 
+    # Show an alarm
+    # if int(clock_seconds) == 45:
+    #    alarm('I am alarming')
+
     # Clear the screen
     display.set_pen(BG)
     display.clear()
 
     # Move and draw the stars
     for star in stars:
-        star.x += star.dx / 10
-        star.y += star.dy / 10
-        if star.x < 0 or star.x > WIDTH or star.y < 0 or star.y > HEIGHT:
-            star.x = MIDX
-            star.y = MIDY
-            star.dx = random.randint(0, STARS_SPEED_MAX * 2) - STARS_SPEED_MAX
-            star.dy = random.randint(0, STARS_SPEED_MAX * 2) - STARS_SPEED_MAX
+        star.move()
+        star.draw()
 
-        display.set_pen(star.pen)
-        display.circle(int(star.x), int(star.y), int(star.r))
+    # Draw the rings
+    drawRing(clock_hours, 70, intervals = 12, small = 6, large = 8, inner = 2)
+    drawRing(clock_minutes, 85)
+    drawRing(clock_seconds, 100)
 
-    # Draw the hours ring
-    for dot in range(0, 12):
-        s1 = dot / 12
-        angle = math.pi * 2 * s1 - math.pi / 2
-        x = MIDX + math.cos(angle) * 70
-        y = MIDY + math.sin(angle) * 70
-
-        if dot >= clock_hours:
-            display.set_pen(GREEN)
-        else:
-            display.set_pen(PURPLE)
-
-        if dot == clock_hours:
-            display.circle(int(x), int(y), 6)
-        else:
-            display.circle(int(x), int(y), 3)
-
-        display.set_pen(BG)
-        display.circle(int(x), int(y), 1)
-
-    # Draw the minutes ring
-    for dot in range(0, 60):
-        s1 = dot / 60
-        angle = math.pi * 2 * s1 - math.pi / 2
-        x = MIDX + math.cos(angle) * 85
-        y = MIDY + math.sin(angle) * 85
-
-        if dot >= clock_minutes:
-            display.set_pen(GREEN)
-        else:
-            display.set_pen(PURPLE)
-
-        if dot == clock_minutes:
-            display.circle(int(x), int(y), 6)
-        else:
-            display.circle(int(x), int(y), 3)
-
-        display.set_pen(BG)
-        display.circle(int(x), int(y), 1)
-
-    # Draw the seconds ring
-    for dot in range(0, 60):
-        s1 = dot / 60
-        angle = math.pi * 2 * s1 - math.pi / 2
-        x = MIDX + math.cos(angle) * 100
-        y = MIDY + math.sin(angle) * 100
-
-        if dot >= clock_seconds:
-            display.set_pen(GREEN)
-        else:
-            display.set_pen(PURPLE)
-
-        if dot == clock_seconds:
-            display.circle(int(x), int(y), 6)
-        else:
-            display.circle(int(x), int(y), 3)
-
-        display.set_pen(BG)
-        display.circle(int(x), int(y), 1)
-
-    # Draw the seconds hand
-    display.set_pen(GREEN)
-    s1 = clock_seconds / 60
-    angle = math.pi * 2 * s1 - math.pi / 2
-    x = MIDX + math.cos(angle) * 90
-    y = MIDY + math.sin(angle) * 90
-    display.line(MIDX, MIDY, int(x), int(y))
-
-    # Draw the minutes hand
-    s1 = clock_minutes / 60
-    angle = math.pi * 2 * s1 - math.pi / 2
-    x = MIDX + math.cos(angle) * 75
-    y = MIDY + math.sin(angle) * 75
-    display.line(MIDX, MIDY, int(x), int(y))
-
-    # Draw the hours hand
-    s1 = clock_hours / 12
-    angle = math.pi * 2 * s1 - math.pi / 2
-    x = MIDX + math.cos(angle) * 60
-    y = MIDY + math.sin(angle) * 60
-    display.line(MIDX, MIDY, int(x), int(y))
+    # Draw the hands
+    drawHand(clock_seconds, 90)
+    drawHand(clock_minutes, 75)
+    drawHand(clock_hours, 60, 12)
 
     # Draw the black hole
     display.set_pen(BLACK)
@@ -155,18 +175,18 @@ while True:
 
     # Draw the time
     display.set_pen(GREEN)
-    text = "{clock_hours}:{clock_minutes}:{clock_seconds}".format(clock_hours=clock_hours, clock_minutes=clock_minutes, clock_seconds=clock_seconds)
-    width = display.measure_text(text, 2)
-    xpos = int( (240 - width) / 2)
+    text = "{clock_hours:d}:{clock_minutes:02d}:{clock_seconds:02d}".format(clock_hours=clock_hours, clock_minutes=clock_minutes, clock_seconds=clock_seconds)
+    twidth = display.measure_text(text, 2)
+    xpos = int((WIDTH - twidth) / 2)
 
     display.set_pen(BG)
-    display.rectangle(xpos-2, 75-2, width + 4, 20)
+    display.rectangle(xpos - 2, 85 - 2, twidth + 4, 20)
 
     display.set_pen(GREEN)
-    display.text(text, xpos, 75, 0, 2)
+    display.text(text, xpos, 85, 0, 2)
 
     # Update the display
     display.update()
 
     # Sleep a while
-    time.sleep(1 / FPS)
+    utime.sleep(1 / FPS)
